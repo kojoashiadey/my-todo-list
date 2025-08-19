288769d465ce6d17296456321dcad93dd9a2af2a
kojoashiadey <161717306+kojoashiadey@users.noreply.github.com>
2025-08-19 21:18:09 +0000

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,12 @@
 import TodoItem from "./TodoItem";
 
 export default function TodoList({ todos, toggleTodo, deleteTodo }) {
+  if (todos.length === 0) {
+    return <p className="text-gray-500 italic mt-4">No tasks yet. Add one above 👆</p>;
+  }
+
   return (
-    <ul>
+    <ul className="divide-y divide-gray-200 mt-4">
       {todos.map((todo) => (
         <TodoItem
           key={todo.id}
