5f9109779251ce16aeed98ee42a06031f8b50879
kojoashiadey <161717306+kojoashiadey@users.noreply.github.com>
2025-08-26 21:37:00 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,7 @@
-import React, { useState } from "react";
-import TodoList from "./components/TodoList";
-import TodoForm from "./components/TodoForm";
-
 export default function App() {
-  const [todos, setTodos] = useState([]);
-
-  const addTodo = (text) => {
-    if (text.trim() === "") return;
-    setTodos([...todos, { id: Date.now(), text, completed: false }]);
-  };
-
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
-  };
-
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
-  };
-
   return (
-    <div style={{ maxWidth: "400px", margin: "40px auto", textAlign: "center" }}>
+    <div>
       <h1>My Todo List</h1>
-      <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </div>
-  );
+  )
 }
