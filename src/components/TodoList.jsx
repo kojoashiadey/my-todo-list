779ae44bcd6b197fac00b6996c34447f4b35fad3
kojoashiadey <161717306+kojoashiadey@users.noreply.github.com>
2025-08-26 21:26:33 +0000

    Create TodoList.jsx

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+export default function TodoList({ todos, toggleTodo, deleteTodo }) {
+  return (
+    <ul style={{ listStyle: "none", padding: 0 }}>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
+  );
+}
